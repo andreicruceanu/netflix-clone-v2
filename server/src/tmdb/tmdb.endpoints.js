bca4697df526d57564443e17f782ef9ac798ff69
andreicruceanu <cruceanuandrei10@gmail.com>
2023-09-02 22:14:20 +0300

    init backend netflix-clone

---
+++ server/src/tmdb/tmdb.endpoints.js
@@ -0,0 +1,24 @@
+import tmdbConfig from "./tmdb.config";
+
+const tmbdEndpoints = {
+  mediaList: ({ mediaType, mediaCategory, page }) =>
+    tmdbConfig.getUrl(`${mediaType}/${mediaCategory}`, page),
+  mediaDetail: ({ mediaType, mediaId }) =>
+    tmdbConfig.getUrl(`${mediaType}/${mediaId}`),
+  mediaGenres: ({ mediaType }) => tmdbConfig.getUrl(`genre/${mediaType}/list`),
+  mediaCredits: ({ mediaType, mediaId }) =>
+    tmdbConfig.getUrl(`${mediaType}/${mediaId}/credits`),
+  mediaVideos: ({ mediaType, mediaId }) =>
+    tmdbConfig.getUrl(`${mediaType}/${mediaId}/videos`),
+  mediaRecommend: ({ mediaType, mediaId }) =>
+    tmdbConfig.getUrl(`${mediaType}/${mediaId}/recommandations`),
+  mediaImages: ({ mediaType, mediaId }) =>
+    tmdbConfig.getUrl(`${mediaType}/${mediaId}/images`),
+  mediaSearch: ({ mediaType, query, page }) =>
+    tmdbConfig.getUrl(`${mediaType}/${mediaId}`, { query, page }),
+  personDetail: ({ personId }) => tmdbConfig.getUrl(`person/${personId}`),
+  personMedias: ({ personId }) =>
+    tmdbConfig.getUrl(`person/${personId}/combined_credits`),
+};
+
+export default tmbdEndpoints;
