dd0197171d379020b401a8d87e4f557dcee29b2b
andreicruceanu <cruceanuandrei10@gmail.com>
2023-09-04 00:43:10 +0300

    get list movie Api completed

---
+++ server/src/controllers/media.controller.js
@@ -0,0 +1,21 @@
+import responseHandler from "../handlers/response.handler.js";
+import tmdbApi from "../tmdb/tmdb.api.js";
+const getList = async (req, res) => {
+  try {
+    const { page } = req.body;
+
+    const { mediaType, mediaCategory } = req.params;
+
+    const response = await tmdbApi.mediaList({
+      mediaType,
+      mediaCategory,
+      page,
+    });
+
+    responseHandler.ok(res, response);
+  } catch {
+    responseHandler.error(res);
+  }
+};
+
+export default { getList };
