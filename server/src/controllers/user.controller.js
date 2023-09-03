2709ee5841a0218d3bac4b5f84c5210a324b41e6
andreicruceanu <cruceanuandrei10@gmail.com>
2023-09-03 18:56:58 +0300

    signup Api completed

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,5 +1,7 @@
 import responseHandler from "../handlers/response.handler.js";
+import userModel from "../models/user.model.js";
 import validateDataFromUser from "../utils/userValidation.js";
+import generateToken from "../utils/generateToken.js";
 
 const signup = async (req, res) => {
   try {
@@ -14,8 +16,34 @@ const signup = async (req, res) => {
       );
     }
 
-    return res.status(200).json({});
-  } catch (error) {}
+    const checkUser = await userModel.findOne({ email });
+
+    if (checkUser) {
+      return responseHandler.badrequest(
+        res,
+        "The email already exists. Add another email address!"
+      );
+    }
+
+    const user = new userModel();
+
+    user.firstName = firstName;
+    user.lastName = lastName;
+    user.email = email;
+    user.setPassword(password);
+
+    await user.save();
+
+    const token = generateToken(user.id);
+
+    responseHandler.created(res, {
+      ...user._doc,
+      id: user.id,
+      token,
+    });
+  } catch (error) {
+    responseHandler.error(res);
+  }
 };
 
 export default { signup };
