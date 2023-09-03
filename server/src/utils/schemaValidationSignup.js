c331ac7e2e0fe2f3097dea704d122625316928d3
andreicruceanu <cruceanuandrei10@gmail.com>
2023-09-03 18:25:52 +0300

    validation Signup API

--- server/src/utils/schemaValidationSignup.js
+++ server/src/utils/schemaValidationSignup.js
@@ -2,12 +2,24 @@ import Joi from "joi";
 
 const handleError = (error) => {
   error.forEach((err) => {
+    console.log(err);
     switch (err.code) {
-      case "string.empty":
+      case "any.required":
         err.message = `${err.local.label} is required`;
         break;
-      default:
+      case "string.empty":
+        err.message = `${err.local.label} is empty`;
         break;
+      case "string.min":
+        err.message = `${err.local.label} length must be at least ${err.local.limit} characters long`;
+        break;
+      case "string.email":
+        err.message = `${err.local.label} is invalid`;
+        break;
+      case "string.pattern.base":
+        err.message = `The password must have at least 8 characters, uppercase, lowercase, a number and a special character`;
+      default:
+        return err;
     }
   });
 
@@ -16,12 +28,13 @@ const handleError = (error) => {
 
 const schemaSignup = Joi.object({
   firstName: Joi.string().min(3).required().error(handleError),
-  lastName: Joi.string().min(2).required().error(handleError),
+  lastName: Joi.string().min(3).required().error(handleError),
   email: Joi.string().email().required().error(handleError),
   password: Joi.string()
     .min(8)
+    .required()
     .regex(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}$/)
-    .required("Parola este Obligatorie"),
+    .error(handleError),
 });
 
 export default schemaSignup;
