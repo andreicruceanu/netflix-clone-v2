7124b891f161869a6162edb70e8083495f64d4f1
andreicruceanu <cruceanuandrei10@gmail.com>
2023-09-05 01:51:30 +0300

    add redux for user

---
+++ client/src/redux/features/userSlice.js
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userSlice = createSlice({
+  name: "User",
+  initialState: {
+    user: null,
+    listFavorites: [],
+  },
+  reducers: {
+    setUser: (state, action) => {
+      if (action.payload === null) {
+        localStorage.removeItem("jwt_token");
+      } else {
+        if (action.payload.token) {
+          localStorage.setItem("jwt_token", action.payload.token);
+        }
+        state.user = action.payload;
+      }
+    },
+    setListFavorite: (state, action) => {
+      state.listFavorites = action.payload;
+    },
+    addFavorite: (state, action) => {
+      state.listFavorites = [action.payload, ...state.listFavorites];
+    },
+    removeFavorite: (state, action) => {
+      const { mediaId } = action.payload;
+      state.listFavorites = [...state.listFavorites].filter(
+        (movie) => movie.mediaId.toString() !== mediaId.toString()
+      );
+    },
+  },
+});
+
+export const { setUser, setListFavorite, removeFavorite, addFavorite } =
+  userSlice;
+
+export default userSlice.reducer;
