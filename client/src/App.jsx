2b034c31ac61678ea43a95e307d57241baeb6fb4
andreicruceanu <cruceanuandrei10@gmail.com>
2023-09-10 15:37:20 +0300

    ThemeProvider and Topbar bug fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,21 +2,28 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import MainLayout from "./components/layout/MainLayout";
 import routes from "./routes/routes";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { useSelector } from "react-redux";
+import themeConfigs from "./configs/theme.configs";
 const App = () => {
+  const { themeMode } = useSelector((state) => state.themeMode);
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          {routes.map((route, index) =>
-            route.index ? (
-              <Route index key={index} element={route.element} />
-            ) : (
-              <Route path={route.path} key={index} element={route.element} />
-            )
-          )}
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={themeConfigs.custom({ mode: themeMode })}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            {routes.map((route, index) =>
+              route.index ? (
+                <Route index key={index} element={route.element} />
+              ) : (
+                <Route path={route.path} key={index} element={route.element} />
+              )
+            )}
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,12 @@
+import { Box } from "@mui/material";
 import React from "react";
-
+import bg from "../assets/images/home.jpg";
 const Home = () => {
-  return <div>B</div>;
+  return (
+    <Box sx={{ width: "100vw", height: "100vh", background: "#fff" }}>
+      <img src={bg} alt="" width={"100%"} />
+    </Box>
+  );
 };
 
 export default Home;
