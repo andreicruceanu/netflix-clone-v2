2de0258358efbeb86ebb6985e948e28b2bcd51d5
andreicruceanu <cruceanuandrei10@gmail.com>
2023-09-18 10:44:45 +0300

    add AppState store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./features/userSlice";
 import themeModeSlice from "./features/themeModeSlice";
 import authModalSlice from "./features/authModalSlice";
+import appStateSlice from "./features/appStateSlice";
 
 const store = configureStore({
   reducer: {
     user: userSlice,
     themeMode: themeModeSlice,
     authModal: authModalSlice,
+    appState: appStateSlice,
   },
 });
 export default store;
