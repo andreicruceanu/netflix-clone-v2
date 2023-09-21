1ea3b87866483fa209e9d6a562c8ce31a5844704
andreicruceanu <cruceanuandrei10@gmail.com>
2023-09-22 00:39:25 +0300

    add trending api

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import userRoute from "./user.route.js";
 import mediaRoute from "./media.route.js";
+import trendingRoute from "./trending.route.js";
 const router = express.Router();
 
 router.use("/user", userRoute);
+router.use("/trending", trendingRoute);
 router.use("/:mediaType", mediaRoute);
 
 export default router;
